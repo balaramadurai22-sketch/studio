84e2459c705eda04f980917e455b309cca3c6295
bala <balaramadurai22@gmail.com>
2025-10-03 10:31:27 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -1,4 +1,5 @@
 "use client";
+import * as React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { user, type Message } from "@/lib/data";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
